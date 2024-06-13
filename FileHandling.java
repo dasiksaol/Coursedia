e93ec8bc8bb856b2b6baba525f77c282e7f80429
dasiksaol <2023218ol@aupp.edu.kh>
2024-06-13 10:37:35 +0700

    test

---
+++ FileHandling.java
@@ -0,0 +1,9 @@
+import java.io.File;
+
+public class FileHandling {
+    File file = new File("C:\\Users\\User\\Documents\\Coursedia File\\CoursediaData.txt");
+
+    public void Read(){
+        
+    }
+}
